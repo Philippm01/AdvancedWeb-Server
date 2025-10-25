86dc56ed7823719e07ed28a92a808ca0a529f899
philippm01 <philippmuro@gmail.com>
2025-10-24 18:15:22 -0700

    Week 9 (fr)

--- AdvancedWeb-Server/Controllers/WeatherForecastController.cs
+++ AdvancedWeb-Server/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdvancedWeb_Server.Controllers
@@ -18,6 +19,7 @@ namespace AdvancedWeb_Server.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
